281135060566bb4b2b160d2dfddf7eb5abf2661b
Adil <143977779+Adil601@users.noreply.github.com>
2025-02-01 01:20:00 +0530

    Update script.js

--- Pixel Gallery/script.js
+++ Pixel Gallery/script.js
@@ -1,4 +1,4 @@
-const accessKey = 'EPaacMtQG6dZ1ONOoHHUIDk2i_XCRVFM2BUD_9jEHwU'; 
+const accessKey = 'ENTER_YOUR_API_KEY_HERE'; 
 
 document.getElementById("fetchBtn").addEventListener("click", fetchImages);
 document.getElementById("themeToggle").addEventListener("click", toggleTheme);
